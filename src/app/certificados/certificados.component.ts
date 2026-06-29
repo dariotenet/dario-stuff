dbdbf7ce7066b98709c8db7665375b968ee4c37e
wachitorico69 <morenodario919@gmail.com>
2026-06-28 20:05:57 -0600

    cambiar nombre de certificados porque eran muy largos

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -12,7 +12,7 @@ export class CertificadosComponent {
       titulo: 'AWS Academy Graduate - Cloud Foundations',
       emisor: 'AWS Academy',
       fecha: '2026',
-      archivo: 'assets/certificados/AWS_Academy_Graduate___Cloud_Foundations___Training_Badge_Badge20260427-32-lfz28n.pdf'
+      archivo: 'assets/certificados/aws-cloud-foundations.pdf'
     },
     {
       titulo: 'Mendix Rapid Developer Certification',
@@ -36,7 +36,7 @@ export class CertificadosComponent {
       titulo: 'Introducción a Cisco Packet Tracer',
       emisor: 'Cisco Networking Academy',
       fecha: '2025',
-      archivo: 'assets/certificados/Getting_Started_with_Cisco_Packet_Tracer_certificate_al351261-edu-uaa-mx_ba229475-ea85-4ac0-bf1f-42e4ed048396.pdf'
+      archivo: 'assets/certificados/cisco-packet-tracer.pdf'
     }
   ];
 }
